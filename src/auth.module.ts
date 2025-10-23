e639c28688a8786b1420bbef31f7aa70e1a0e49f
ValentinHer <valentinhernandezhernandez18@gmail.com>
2025-10-22 18:25:53 -0600

    [ADD] archivo .env.example agregado

--- src/auth.module.ts
+++ src/auth.module.ts
@@ -2,15 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({isGlobal: true}),
     ClientsModule.register([
       {
         name: "USER_SERVICE",
         transport: Transport.TCP,
         options: {
-          host: "127.0.0.1",
+          host: process.env.USER_SERVICE_HOST,
           port: 4000
         }
       }
